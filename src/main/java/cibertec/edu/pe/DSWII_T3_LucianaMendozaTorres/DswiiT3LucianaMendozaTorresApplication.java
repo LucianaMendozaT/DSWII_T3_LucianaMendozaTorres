ee40d4c20312b2d3f99436fc9dd2ff0e836c5cbe
LucianaMendozaT <lucianamend0124@gmail.com>
2023-11-25 21:54:28 -0500

    primer commit

---
+++ src/main/java/cibertec/edu/pe/DSWII_T3_LucianaMendozaTorres/DswiiT3LucianaMendozaTorresApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_LucianaMendozaTorres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3LucianaMendozaTorresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3LucianaMendozaTorresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DSWII_T3_LucianaMendozaTorres/DswiiT3LucianaMendozaTorresApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_LucianaMendozaTorres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3LucianaMendozaTorresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
